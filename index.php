f7a59329d0a0129b0633d5d70e0470d46da0f3eb
khoursaaf <khoursa@gmail.com>
2018-11-19 10:03:27 +0100

    fist commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Exercise</title>
+    </head>
+    <body>
+      <p>Créer une variable de type int. L'initialiser avec rien. Afficher sa valeur.</p>
+      <p><?php $int = NULL; echo $int; ?></p>
+      <p>Donner une valeur à cette variable et l'afficher.</p>
+      <p><?php $int = 3; echo $int; ?></p>
+    </body>
+</html>
