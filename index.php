f01009851478d688e72511b6b2a2dc1a7462eae8
khoursaaf <khoursa@gmail.com>
2018-11-20 16:20:00 +0100

    correction

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Exercise</title>
-    </head>
-    <body>
-      <p>Créer une variable de type int. L'initialiser avec rien. Afficher sa valeur.</p>
-      <p><?php $int = NULL; echo $int; ?></p>
-      <p>Donner une valeur à cette variable et l'afficher.</p>
-      <p><?php $int = 3; echo $int; ?></p>
-    </body>
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercise</title>
+  </head>
+  <body>
+    <p>Créer une variable de type int. L'initialiser avec rien. Afficher sa valeur.</p>
+    <?php $int = NULL; ?>
+    <p><?php echo $int; ?></p>
+    <p>Donner une valeur à cette variable et l'afficher.</p>
+    <?php $int = 3; ?>
+    <p><?php echo $int; ?></p>
+  </body>
 </html>
